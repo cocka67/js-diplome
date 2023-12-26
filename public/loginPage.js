85ccc78cda3d53d88cf148aa02aa173c6f84fe5c
cocka67 <jdanoffk@yandex.ru>
2023-12-26 19:51:56 +0500

    da

---
+++ public/loginPage.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// Создаем объект класса UserForm
+const userForm = new UserForm();
+
+// Устанавливаем свойство loginFormCallback с нужной функциональностью
+userForm.loginFormCallback = (data) => {
+    // Выполняем запрос на сервер для попытки авторизации пользователя
+    ApiConnector.login(data, (response) => {
+        console.log(response); // Выводим ответ сервера в консоль для отладки
+
+        if (response.success) {
+            // Успешная авторизация
+            console.log("Авторизация успешна");
+            // Обновляем страницу
+            userForm.loginFormAction(data);
+            location.reload();
+        } else {
+            // Ошибка авторизации
+            console.error("Ошибка авторизации: " + response.error);
+            // Выводим ошибку в окно для ошибок
+            userForm.setLoginErrorMessage(response.error);
+        }
+    });
+};
+
+userForm.registerFormCallback = (data) => {
+    // Выполняем запрос на сервер для попытки авторизации пользователя
+    ApiConnector.register(data, (response) => {
+        console.log(response); // Выводим ответ сервера в консоль для отладки
+
+        if (response.success) {
+            // Успешная авторизация
+            console.log("Вы успешно зарегистрировались");
+            // Обновляем страницу
+            userForm.registerFormAction(data);
+            location.reload();
+        } else {
+            // Ошибка авторизации
+            console.error("Ошибка регистрации: " + response.error);
+            // Выводим ошибку в окно для ошибок
+            userForm.setRegisterErrorMessage(response.error);
+        }
+    });
+};
